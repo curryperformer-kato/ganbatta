6a3dc6f536b3d86e8b0b5e8b6e6aae5d9eebcb84
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-09-16 17:09:32 +0000

    init unicorn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'enigmatic-sands-40891.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '13.114.177.208' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
