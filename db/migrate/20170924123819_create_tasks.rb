5b3ee0f255ff89f6fb043948b36af7fb7637479d
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-09-24 02:34:32 +0000

    first_commit

--- db/migrate/20170924123819_create_tasks.rb
+++ db/migrate/20170924123819_create_tasks.rb
@@ -2,8 +2,8 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.integer :task_id 
-      t.text :name
-      t.text :memo
+      t.string :name
+      t.string :memo
 
       t.timestamps null: false
     end
