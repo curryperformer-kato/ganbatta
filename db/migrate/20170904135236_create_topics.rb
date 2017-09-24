44be2115d55a3cb3469f7197140399b63aaff432
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-09-24 08:18:50 +0000

    first_commit

--- db/migrate/20170904135236_create_topics.rb
+++ db/migrate/20170904135236_create_topics.rb
@@ -2,8 +2,6 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :title
-      t.string :name
-      t.string :memo
       
       t.timestamps null: false
     end
