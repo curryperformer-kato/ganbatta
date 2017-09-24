e9bd85fdc71456a800203d853425d06364b1e80d
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-09-24 13:55:39 +0000

    first_commit

--- db/migrate/20170924123820_add_task_id_to_topics.rb
+++ db/migrate/20170924123820_add_task_id_to_topics.rb
@@ -1,5 +1,7 @@
 class AddTaskIdToTopics < ActiveRecord::Migration
   def change
     add_column :topics, :task_id, :integer
+    add_column :topics, :name, :string
+    add_column :topics, :memo, :string
   end
 end
