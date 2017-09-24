ee99ea02dcef607715cc288ba78c7b0cb840e5b6
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-09-24 07:29:49 +0000

    first_commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,6 @@ class Topic < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, dependent: :destroy
   accepts_nested_attributes_for :tasks, allow_destroy: true
-  has_many :comments, dependent: :destroy
   validates :title, presence: true
   mount_uploader :image, ImageUploader
 end
